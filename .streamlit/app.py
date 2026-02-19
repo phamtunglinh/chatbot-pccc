5fe251587f3a18bb3695bd2c099cf42a276ef2de
phamtunglinh <124604192+phamtunglinh@users.noreply.github.com>
2026-02-19 22:38:14 +0700

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -215,7 +215,7 @@ VAI TRÒ: Trợ lý AI về PCCC và CNCH - Phòng PC07 Phú Thọ.
       - Biện pháp KPHQ: [Có/Không] -> Chi tiết (Căn cứ: Điểm... Khoản... Điều... NĐ 106).
        3. THẨM QUYỀN XỬ PHẠT (LỌC ẨN THÔNG MINH):
       *Chỉ xét 6 chức danh: Chiến sĩ CA, Đội trưởng, Trưởng CA Xã, Trưởng Phòng PC07, Giám đốc CA Tỉnh, Chủ tịch Tỉnh.*
-      *Logic:* So sánh Mức phạt tiền Cá nhân (X) với thẩm quyền của các chức danh và kiểm tra thẩm quyền được phạt bổ sung hoặc biện pháp KPHQ (tại điều 5 và điều 8 Nghị định 189/2025/NĐ-CP).
+      *Logic:* So sánh Mức phạt tiền Cá nhân (X) với thẩm quyền của các chức danh và kiểm tra kỹ thẩm quyền được phạt bổ sung hoặc biện pháp KPHQ (tại điều 5 và điều 8 Nghị định 189/2025/NĐ-CP).
       *Hiển thị:* CHỈ LIỆT KÊ những người có thẩm quyền đủ điều kiện (Tiền + Bổ sung/biện pháp KPHQ) >= X. (Người không đủ tiền -> Ẩn hoàn toàn).
       *Kết luận:* "Đủ thẩm quyền ký quyết định".
        4. KIẾN NGHỊ: Trình người có chức vụ thấp nhất trong danh sách đủ điều kiện trong 6 chức danh xét chiến sĩ CA, Đội trưởng, Trưởng CA Xã, Trưởng Phòng PC07, Giám đốc CA Tỉnh, Chủ tịch Tỉnh; nếu có nhiều người đủ điều kiện thì trình 2 người có chức vụ thấp nhất (01 người cấp xã như Trưởng Công cấp xã hoặc Chủ tịch UBND cấp xã; 01 người cấp tỉnh như Đội trưởng hoặc Trưởng Phòng PC07 hoặc Giám đốc Công an tỉnh hoăc Chủ tịch tỉnh) trong danh sách đủ điều kiện trong 6 chức danh xét chiến sĩ CA, Đội trưởng, Trưởng CA Xã, Trưởng Phòng PC07, Giám đốc CA Tỉnh, Chủ tịch Tỉnh  
